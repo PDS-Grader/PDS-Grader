85f4cfbf5838ab1df7b076ec8bbb20e1fe10b8cc
PakinDioxide <pakindioxide@gmail.com>
2024-07-18 17:45:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ cookie_manager = get_manager()
 if 'logged_in' not in st.session_state:
     st.session_state['logged_in'] = cookie_manager.get(cookie="patumwandemonstrationschool_71") != ""
 
-# st.session_state['logged_in'] = not cookie_manager.get(cookie="patumwandemonstrationschool_71") == ""
+st.session_state['logged_in'] = cookie_manager.get(cookie="patumwandemonstrationschool_71") != ""
 
 if 'username' not in st.session_state:
     st.session_state['username'] = cookie_manager.get(cookie="patumwandemonstrationschool_71")
@@ -28,7 +28,7 @@ if 'username' not in st.session_state:
 if st.session_state['username'] == "":
     st.session_state['logged_in'] = False
     
-# st.session_state['username'] = cookie_manager.get(cookie="patumwandemonstrationschool_71")
+st.session_state['username'] = cookie_manager.get(cookie="patumwandemonstrationschool_71")
 
 # Function to initialize user database
 def init_user_db():
